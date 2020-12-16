a422d7dafb36ed8648786ca0750b516516beda07
Nikhil Naik <nnaik@mail.usf.edu>
2020-12-16 00:34:46 -0500

    changes in index file

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,21 @@
-import React from "react"
+import React,  { useEffect } from "react"
 import Us from "../../components/Us"
 import SimpleSlider from "../../components/SimpleSlider"
 import Story from "../../components/Story"
 import Welcome from "../../components/Welcome"
-import WeddingParty from "../../components/WeddingParty"
+import MaidAndMen from "../../components/MaidAndMen"
 import Events from "../../components/Events"
 import Header from "../../components/Header"
 import MobileMenu from "../../components/MobileMenu"
 import CountdownWidget from "../../components/CountdownWidget"
+import {TITLE} from "../../constants/headings"
+
 
 const Home = () => {
+    useEffect(() => {
+        document.title = TITLE
+    }, []);
+
     return (
         <div>
             <Header />
@@ -19,7 +25,7 @@ const Home = () => {
             <Us />
             <Welcome />
             <Story />
-            <WeddingParty />
+            <MaidAndMen />
             <Events />
         </div>
     )
