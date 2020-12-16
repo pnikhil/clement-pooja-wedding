dbabc012f20119f57498e47c1d62561c08010b5d
Nikhil Naik <nnaik@mail.usf.edu>
2020-12-15 23:32:10 -0500

    Updates

---
+++ precache-manifest.4ee513f1397e1fee69f1942a396b2238.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3145fe4d096421e74cd00af7aa7a686b",
+    "url": "/clement-pooja-wedding/index.html"
+  },
+  {
+    "revision": "fd4cf554e82ee75652e6",
+    "url": "/clement-pooja-wedding/static/css/2.9137866d.chunk.css"
+  },
+  {
+    "revision": "a1cbb924f9974fdbd04d",
+    "url": "/clement-pooja-wedding/static/css/main.54ec830f.chunk.css"
+  },
+  {
+    "revision": "fd4cf554e82ee75652e6",
+    "url": "/clement-pooja-wedding/static/js/2.6ca42554.chunk.js"
+  },
+  {
+    "revision": "a1cbb924f9974fdbd04d",
+    "url": "/clement-pooja-wedding/static/js/main.1aa23044.chunk.js"
+  },
+  {
+    "revision": "8e3ef6fb6665f23bf706",
+    "url": "/clement-pooja-wedding/static/js/runtime~main.ca100da2.js"
+  },
+  {
+    "revision": "f013f404c481fe4f9756334bfde9e82d",
+    "url": "/clement-pooja-wedding/static/media/anthony-chapel.f013f404.jpg"
+  },
+  {
+    "revision": "e630adbf9b707e893e7b68ce757396b9",
+    "url": "/clement-pooja-wedding/static/media/carter-funny.e630adbf.jpg"
+  },
+  {
+    "revision": "0de21c921ec5bf1d92a5e9d37155bc32",
+    "url": "/clement-pooja-wedding/static/media/carter.0de21c92.jpg"
+  },
+  {
+    "revision": "3ca6aba273e362746e5a591f82c2938c",
+    "url": "/clement-pooja-wedding/static/media/checkmark.3ca6aba2.png"
+  },
+  {
+    "revision": "a8e0aa588be082133ace4bbb674db400",
+    "url": "/clement-pooja-wedding/static/media/clipart111165.a8e0aa58.png"
+  },
+  {
+    "revision": "5eef5fd707e77b13c793b99a14008ee1",
+    "url": "/clement-pooja-wedding/static/media/dating.5eef5fd7.jpg"
+  },
+  {
+    "revision": "159fa52767531adf5e366d862b9411cc",
+    "url": "/clement-pooja-wedding/static/media/enagement.159fa527.jpg"
+  },
+  {
+    "revision": "0dc18c330dec2703d59b2434f8677ece",
+    "url": "/clement-pooja-wedding/static/media/engagement-2.0dc18c33.jpg"
+  },
+  {
+    "revision": "b37169b975a3eaa14c46b51535aab7a9",
+    "url": "/clement-pooja-wedding/static/media/engagement-3.b37169b9.jpg"
+  },
+  {
+    "revision": "dcfae70e9d073b8042e30114a87a787b",
+    "url": "/clement-pooja-wedding/static/media/engagement.dcfae70e.jpg"
+  },
+  {
+    "revision": "ffc66dc3c24ded7e0ba60d8472bc4523",
+    "url": "/clement-pooja-wedding/static/media/grace-funny.ffc66dc3.jpg"
+  },
+  {
+    "revision": "9fb433cc26635c5455f5935b58f69e90",
+    "url": "/clement-pooja-wedding/static/media/grace.9fb433cc.jpg"
+  },
+  {
+    "revision": "1d81efc933726935de59a2bc40d35280",
+    "url": "/clement-pooja-wedding/static/media/meeting.1d81efc9.jpg"
+  },
+  {
+    "revision": "87dae995d9cb3ec9ed4eba8672c4e8e4",
+    "url": "/clement-pooja-wedding/static/media/rebekah-funny.87dae995.jpg"
+  },
+  {
+    "revision": "964d0e3ef727e60c0bb0991fa9ceda0c",
+    "url": "/clement-pooja-wedding/static/media/rebekah.964d0e3e.jpg"
+  },
+  {
+    "revision": "13607e41f48addab194030861b7afc7e",
+    "url": "/clement-pooja-wedding/static/media/ross-funny.13607e41.jpg"
+  },
+  {
+    "revision": "d6db9f780badbbfd2fb0442137f9db54",
+    "url": "/clement-pooja-wedding/static/media/ross.d6db9f78.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/clement-pooja-wedding/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/clement-pooja-wedding/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/clement-pooja-wedding/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/clement-pooja-wedding/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "c1e518fe8fd562e74b7e60ced29e7e22",
+    "url": "/clement-pooja-wedding/static/media/springdale-station-inside.c1e518fe.jpg"
+  }
+]);
