92c0e15c07d85f0f1a14239d62cf8ef6255c009d
Nikhil Naik <nnaik@mail.usf.edu>
2020-12-25 17:51:42 -0600

    added dynamic data as constants

--- src/constants/headings.js
+++ src/constants/headings.js
@@ -1,11 +1,6 @@
 const PAGE_TITLE = "Pooja & Clement's Wedding";
 const NAME_FULL = "Pooja Vincia D'Souza and Clement Almeida";
 const NAME_SHORT = 'Pooja & Clement';
-const FOOTER_HEADING = 'We can\'t wait to celebrate with you!';
+const FOOTER_HEADING = "We can't wait to celebrate with you!";
 
-export {
-    PAGE_TITLE,
-    NAME_FULL,
-    NAME_SHORT,
-    FOOTER_HEADING
-};
+export { PAGE_TITLE, NAME_FULL, NAME_SHORT, FOOTER_HEADING };
